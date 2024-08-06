cdd9d5c7ee7eb1d07980ea366ac485e0a260db90
GMuthy <30766349+GMuthy@users.noreply.github.com>
2024-08-06 15:52:05 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,22 @@
+package looping;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=1;k<=4-i+1;k++)
+			{
+				System.out.print(" ");
+			}
+				for(int j=1;j<=i;j++) 
+				{
+			System.out.print("*");
+
+		}
+			System.out.println();
+		}
+		}
+
+
+}
